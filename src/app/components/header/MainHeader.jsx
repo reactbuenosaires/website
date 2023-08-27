315d407a25513dd6c88798304f02c5b341f9ccfb
naniitevez <nicole.tevez.97@gmail.com>
2023-08-27 02:56:37 -0300

    modifico nombre de archivo navbar

--- src/app/components/header/MainHeader.jsx
+++ src/app/components/header/MainHeader.jsx
@@ -1,9 +1,9 @@
 import menuIcon from '../../../../public/bars-solid.svg'
-import style from '../navBar/navBar.module.css'
+import style from './MainHeader.module.css'
 import Image from 'next/image'
 
 
-export default function NavBar() {
+export default function MainHeader() {
   return (
     <>
       <div className={style.nav}>
