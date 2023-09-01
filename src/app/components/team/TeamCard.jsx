0d6108c6c70fd37512b3fc6511b8f1c662d28fd3
Gabriel <loveragabriel20@gmail.com>
2023-09-01 19:50:15 -0300

    font-size title actualizado

--- src/app/components/team/TeamCard.jsx
+++ src/app/components/team/TeamCard.jsx
@@ -1,28 +1,28 @@
-import Image from "next/image"
-import style from '@/app/components/team/reactTeam.module.css'
+import Image from "next/image";
+import style from "@/app/components/team/reactTeam.module.css";
 export default function TeamCard(props) {
-    return (
-        <div >
-            <div className={style.teamMember}>
-                <Image 
-                  className={style.borderPic} 
-                  src={props.urlPic} 
-                  width={80} 
-                  alt='text' 
-                  height={80}
-                />
-                <div className={style.memberData}>
-                      <h5>{props.name}</h5>
-                      <p>{props.role}</p>
-                        <Image 
-                          className={style.icon}
-                          src={props.networkUrl} 
-                          alt='text' 
-                          width={15} 
-                          height={15} 
-                        />
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={style.teamMember}>
+        <Image
+          className={style.borderPic}
+          src={props.urlPic}
+          width={80}
+          alt="text"
+          height={80}
+        />
+        <div className={style.memberData}>
+          <h5>{props.name}</h5>
+          <p>{props.role}</p>
+          <Image
+            className={style.icon}
+            src={props.networkUrl}
+            alt="text"
+            width={15}
+            height={15}
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
