4f1c5458123c80ef64ef83d3cefd8171672ef100
Gabriel <loveragabriel20@gmail.com>
2023-09-16 00:34:13 -0300

    YT component serverSide + mejoro calidad images

--- src/app/components/youtube/Youtube.jsx
+++ src/app/components/youtube/Youtube.jsx
@@ -1,5 +1,3 @@
- 'use client'
-import React, { useEffect, useState } from "react";
 import Title from "../Title";
 import style from "@/app/components/youtube/youtube.module.css"
 import Image from "next/image";
@@ -8,24 +6,11 @@ dotenv.config();
 
 const apiKey=process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 
-async function getYtVideos() {
-  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UC8lMWXElwhflZxWqsE6BuPQ&part=snippet,id&order=date&maxResults=3`);
-  const data = await response.json();
-  return data.items || [];
-}
-
-export default function Youtube() {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const videoData = await getYtVideos();
-      setVideos(videoData); // Set the videos array directly.
-      console.log(videoData);
-    }
-
-    fetchData();
-  }, []); // Empty dependency array to run this effect once, similar to componentDidMount
+export default async function Youtube() {
+  const res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UC8lMWXElwhflZxWqsE6BuPQ&part=snippet,id&order=date&maxResults=3` );
+  const json_Data = await res.json();
+  const videos  = json_Data.items
+  console.log(videos.snippe)
 
   return (
     <div>
@@ -36,9 +21,9 @@ export default function Youtube() {
           <div className={style.youtube_card} >
             <Image className={style.image}
               src={video.snippet.thumbnails.high.url} 
-              alt={video.snippet.thumbnails.title}
-              width={100}
-              height={100}
+              alt='Video de Youtube'
+              width={video.snippet.thumbnails.high.width}
+              height={video.snippet.thumbnails.high.height}
             />
           <p className={style.titulo} >{video.snippet.title}</p>
           </div>
