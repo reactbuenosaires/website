7a2d98313ba1084558bc64e3cf38c25b6d8689c9
Gabriel <loveragabriel20@gmail.com>
2023-09-19 19:12:03 -0300

    ajuste de formato youtuComponente

--- src/app/components/youtube/Youtube.jsx
+++ src/app/components/youtube/Youtube.jsx
@@ -1,35 +1,42 @@
 import Title from "../Title";
-import style from "@/app/components/youtube/youtube.module.css"
+import style from "@/app/components/youtube/youtube.module.css";
 import Image from "next/image";
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
-const apiKey=process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 
 export default async function Youtube() {
-  const res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UC8lMWXElwhflZxWqsE6BuPQ&part=snippet,id&order=date&maxResults=4` );
+  const res = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UC8lMWXElwhflZxWqsE6BuPQ&part=snippet,id&order=date&maxResults=4`
+  );
   const json_Data = await res.json();
-  const videos  = json_Data.items
-  console.log(videos.snippe)
+  const videos = json_Data.items;
+  console.log(videos.snippe);
 
   return (
     <div className={style.youtube_section}>
       <Title titleDescription="MEETUPS REALIZADAS" />
       <h3 className={style.subtitle}>
-          ¡Descrubí todo el contenido de meetups pasadas en Youtube!
-        </h3>
+        ¡Descrubí todo el contenido de meetups pasadas en Youtube!
+      </h3>
       <div className={style.container}>
         {videos.map((video, index) => (
-          <a className={style.yt_enlaces} href={`https://www.youtube.com/watch?v=${video.id.videoId}`} key={index}>
-          <div className={style.youtube_card} >
-            <Image className={style.image}
-              src={video.snippet.thumbnails.high.url} 
-              alt='Video de Youtube'
-              width={video.snippet.thumbnails.high.width}
-              height={video.snippet.thumbnails.high.height}
-            />
-          <p className={style.titulo} >{video.snippet.title}</p>
-          </div>
+          <a
+            className={style.yt_enlaces}
+            href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+            key={index}
+          >
+            <div className={style.youtube_card}>
+              <Image
+                className={style.image}
+                src={video.snippet.thumbnails.high.url}
+                alt="Video de Youtube"
+                width={video.snippet.thumbnails.high.width}
+                height={video.snippet.thumbnails.high.height}
+              />
+              <p className={style.titulo}>{video.snippet.title}</p>
+            </div>
           </a>
         ))}
       </div>
