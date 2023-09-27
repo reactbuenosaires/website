9c3c9a2007c072b91ab41af456be3da5f242c205
Gabriel <loveragabriel20@gmail.com>
2023-09-27 19:53:36 -0300

    agrego indentacion

--- src/app/components/past_events/fetchYoutubeData.js
+++ src/app/components/past_events/fetchYoutubeData.js
@@ -1,31 +1,31 @@
 export async function fetchYoutubeData(apiKey) {
-    const res = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UC8lMWXElwhflZxWqsE6BuPQ&part=snippet,id&order=date&maxResults=5`
-    );
-    const json_Data = await res.json();
-    return json_Data.items;
-  }
-  
-  export function getListVideoYoutube(data,status) {
-    return data.filter(item => {
-      const liveBroadcastContent = item.snippet.liveBroadcastContent;
-      return liveBroadcastContent === status;
-    }).slice(0, 4);
-  }
+  const res = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UC8lMWXElwhflZxWqsE6BuPQ&part=snippet,id&order=date&maxResults=5`
+  );
+  const json_Data = await res.json();
+  return json_Data.items;
+}
 
-  export function liveContentStatus(data) {
-    if(data[0].snippet.liveBroadcastContent==='upcoming'){
-      return 'upcoming'
-    } else if(data[0].snippet.liveBroadcastContent=== 'live'){
-      return 'live'
-    } else if (data[0].snippet.liveBroadcastContent=== 'none'){
-      return 'none'
-    }
-  }
+export function getListVideoYoutube(data, status) {
+  return data.filter(item => {
+    const liveBroadcastContent = item.snippet.liveBroadcastContent;
+    return liveBroadcastContent === status;
+  }).slice(0, 4);
+}
 
-  export function liveContent(data) {
-    return data[0];
+export function liveContentStatus(data) {
+  if (data[0].snippet.liveBroadcastContent === 'upcoming') {
+    return 'upcoming'
+  } else if (data[0].snippet.liveBroadcastContent === 'live') {
+    return 'live'
+  } else if (data[0].snippet.liveBroadcastContent === 'none') {
+    return 'none'
   }
+}
+
+export function liveContent(data) {
+  return data[0];
+}
 
 
 
