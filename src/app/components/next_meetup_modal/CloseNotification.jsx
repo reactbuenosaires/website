7d60fa20da235ea5222ad60abef558ce57b83e73
Gabriel <loveragabriel20@gmail.com>
2023-09-29 00:41:32 -0300

    ajusto style notification

--- src/app/components/next_meetup_modal/CloseNotification.jsx
+++ src/app/components/next_meetup_modal/CloseNotification.jsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { useState } from "react";
+import style from './notification.module.css'
 
 export default function CloseNotification({ children }) {
   const [isDisplay, setIsDisplay] = useState(true);
   return (
-    <section>
+    <>
       {isDisplay && (
-        <div>
-          <button onClick={() => setIsDisplay(!isDisplay)}>Cerrar</button>
+        <>
+          <p className={style.close_text } onClick={() => setIsDisplay(!isDisplay)}>Cerrar</p>
           {children}
-        </div>
+        </>
       )}
-    </section>
+    </>
   );
 }
