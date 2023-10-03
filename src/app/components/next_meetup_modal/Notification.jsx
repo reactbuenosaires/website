1596412c34b096e496f14e3485c461050fd911c4
Gabriel <loveragabriel20@gmail.com>
2023-10-03 19:12:04 -0300

    alimino componente DataRendered

--- src/app/components/next_meetup_modal/Notification.jsx
+++ src/app/components/next_meetup_modal/Notification.jsx
@@ -1,6 +1,5 @@
 import style from "@/app/components/next_meetup_modal/notification.module.css";
 import CloseNotification from "@/app/components/next_meetup_modal/CloseNotification";
-import DataRedered from "@/app/components/next_meetup_modal/DataRendered";
 import NextMeetUp from './RenderNotificacion'
 
 export default function Notification() {
