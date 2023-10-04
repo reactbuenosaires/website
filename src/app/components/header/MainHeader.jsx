33a0489aaa9198b5ef2c51c5399c657a8cbaaa0a
naniitevez <nicole.tevez.97@gmail.com>
2023-10-04 11:44:34 -0300

    agrego header para mobile y modifico media breaks para responsive

---
+++ src/app/components/header/MainHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./MainHeader.module.css";
+import Image from "next/image";
+import logo from "@/app/assets/React_en_Buenos_Aires_logo.png";
+
+const MainHeader = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.logo_container}>
+        <Image src={logo} height={50} />
+      </div>
+    </header>
+  );
+};
+
+export default MainHeader;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
-import '@/app/global.css';
+import "@/app/global.css";
+import MainHeader from "@/app/components/header/MainHeader";
 import MainFooter from "@/app/components/footer/MainFooter";
-import SocialMediaBanner from './components/social_media_banner/SocialMediaBanner';
+import SocialMediaBanner from "./components/social_media_banner/SocialMediaBanner";
 
 export const metadata = {
-  title: 'React en Buenos Aires',
-}
+  title: "React en Buenos Aires",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
+        <MainHeader />
         {children}
         <MainFooter />
         <SocialMediaBanner />
       </body>
     </html>
-  )
+  );
 }
