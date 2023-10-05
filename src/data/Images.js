96624231a7ef0332fc82b0a70129d5a60cbd33f3
Gabriel <loveragabriel20@gmail.com>
2023-10-05 00:09:03 -0300

    actualizo ptah de imagenes

--- src/data/Images.js
+++ src/data/Images.js
@@ -1,11 +1,11 @@
-import speaker from "@/app/assets/images/speaker.jpg";
-import stage from "@/app/assets/images/conferenceRoom.jpg";
-import people from "@/app/assets/images/people.jpg";
-import code from "@/app/assets/images/code.jpg";
-import speaker_about from "@/app/assets/images/about_images/meetup_speaker.jpeg"
-import stage_about from "@/app/assets/images/about_images/meetup_place.jpeg"
-import people_about from "@/app/assets/images/about_images/meetup_people.jpeg"
-import content_about from "@/app/assets/images/about_images/meetup_content.jpeg"
+import speaker from "@/assets/images/speaker.jpg";
+import stage from "@/assets/images/conferenceRoom.jpg";
+import people from "@/assets/images/people.jpg";
+import code from "@/assets/images/code.jpg";
+import speaker_about from "@/assets/images/about_images/meetup_speaker.jpeg"
+import stage_about from "@/assets/images/about_images/meetup_place.jpeg"
+import people_about from "@/assets/images/about_images/meetup_people.jpeg"
+import content_about from "@/assets/images/about_images/meetup_content.jpeg"
 
 export const aboutUsImages = [
   { description: "Speaker", img: speaker_about },
