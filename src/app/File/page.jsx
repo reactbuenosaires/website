5234307e4254a4ac6802183ea03ba1791946abe6
Gabriel <loveragabriel20@gmail.com>
2023-10-19 01:06:21 -0300

    agrego resto de page para sorteos

---
+++ src/app/File/page.jsx
@@ -0,0 +1,9 @@
+import Title from "../../components/Title"
+import styles from "./file.module.css"
+export default function File(){
+    return(
+        <section className={styles.file_container}>
+        <Title titleDescription='Próximamente'/>
+        </section>
+    )
+}
---
+++ src/app/Registro/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./form.module.css"
+import Title from "../../components/Title"
+export default function Form(){
+    return(     
+           <section className={styles.form_container}>
+            <Title titleDescription="PRÓXIMAMENTE"/>
+           </section>
+
+        )
+}
