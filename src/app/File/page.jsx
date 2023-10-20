77577c2b458c5978f65a3794afae1d949b07d0c5
Gabriel <loveragabriel20@gmail.com>
2023-10-20 01:36:45 -0300

    agrego link sorteos

--- src/app/File/page.jsx
+++
@@ -1,9 +0,0 @@
-import Title from "../../components/Title"
-import styles from "./file.module.css"
-export default function File(){
-    return(
-        <section className={styles.file_container}>
-        <Title titleDescription='Próximamente'/>
-        </section>
-    )
-}
---
+++ src/app/random/page.jsx
@@ -0,0 +1,5 @@
+export default function Random(){
+    return(
+        <p>Hola soy Random</p>
+    )
+}
