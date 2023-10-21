a29f5e89f26bfd849fc5f672997338400d73211a
Gabriel <loveragabriel20@gmail.com>
2023-10-21 20:38:47 -0300

    completo page random

--- src/app/giveaways/random/page.jsx
+++ src/app/giveaways/random/page.jsx
@@ -1,18 +1,36 @@
-'use client'
+"use client";
 import Title from "@/components/Title";
 import styles from "./random.module.css";
+import { useState } from "react";
+
 export default function Random() {
+  const [inputRandom, setInputRandom] = useState("");
+  const [winner, setWinner] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputRandom(e.target.value);
+  };
+
+  const getRandom = () => {
+    const randomValue = Math.floor(Math.random() * inputRandom);
+    setWinner(randomValue);
+  };
+
   return (
     <section className={styles.random_container}>
       <Title titleDescription="RANDOM" />
       <div className={styles.input_container}>
         <p className={styles.text}>Participantes</p>
-        <input type="number" className={styles.input}></input>
+        <input
+          type="number"
+          className={styles.input}
+          onChange={handleInputChange}
+        />
       </div>
-      <button className={styles.random_button}onClick={()=>alert('numero')}>Generar</button>
-      <div className={styles.random_winner}>
-        20
-      </div>
+      <button className={styles.random_button} onClick={getRandom}>
+        Generar
+      </button>
+      {winner && <div className={styles.random_winner}>{winner}</div>}
     </section>
   );
 }
