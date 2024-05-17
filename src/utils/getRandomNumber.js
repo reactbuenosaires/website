3dd09f924ac0266714fcd863fb24a39d617396ed
naniitevez <nicole.tevez.97@gmail.com>
2024-05-17 20:50:36 -0300

    Modifico funcion para obtener numero aleatorio de la api de random.org

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,14 +1,20 @@
-export async function getRandomNumber(maxValue) {
-    try {
-        const response = await fetch(`https://www.random.org/integers/?num=1&min=1&max=${maxValue}&col=1&base=10&format=plain&rnd=new`);
-        const randomWinner = await response.text();
-        const parsedNumber = parseInt(randomWinner);
-        if (!isNaN(parsedNumber)) {
-            return parsedNumber;
-        } else {
-            throw new Error("Invalid response: Not a valid integer");
-        }
-    } catch (error) {
-        console.error("Error:", error);
+export default async function getRandomNumber(maxValue) {
+  try {
+    const response = await fetch(
+      `https://www.random.org/integers/?num=1&min=1&max=${maxValue}&col=1&base=10&format=plain&rnd=new`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const randomWinner = await response.text();
+    const parsedNumber = parseInt(randomWinner, 10);
+    if (!isNaN(parsedNumber)) {
+      return parsedNumber;
+    } else {
+      throw new Error("Invalid response: Not a valid integer");
+    }
+  } catch (error) {
+    console.error("Error fetching random number:", error);
+    return null;
+  }
 }
