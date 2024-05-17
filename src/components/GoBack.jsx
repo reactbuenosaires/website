dc43f1961463b1779e1ad58770a7885f5c0181e4
naniitevez <nicole.tevez.97@gmail.com>
2024-05-17 20:51:41 -0300

    creo componente para volver atras en la navegaion

---
+++ src/components/GoBack.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styles from "./GoBack.module.css";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
+
+const GoBack = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <button onClick={() => router.back()}>
+        <FontAwesomeIcon icon={faCircleLeft} /> <strong>Volver</strong>
+      </button>
+    </div>
+  );
+};
+
+export default GoBack;
