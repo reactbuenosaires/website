56d96ff9bfc43a31be888d5d9f1f0e1c7ab3c8b0
Lara Mateo <asdasd5056@gmail.com>
2026-09-04 19:33:36 -0300

    Add Lara Mateo to team profiles and include new image (#15)

--- src/data/TeamNetworks.js
+++ src/data/TeamNetworks.js
@@ -1,7 +1,6 @@
 import facuProfileImg from "@/assets/team/Facu_facundozurdo.jpg";
 import nanoProfileImg from "@/assets/team/Nano_nanovazquez__.jpg";
-import nickyProfileImg from "@/assets/team/Nicky_naniitevez.png";
-import mateProfileImg from "@/assets/team/Mateo_mateosilguero1.jpg";
+import laraProfileImg from "@/assets/team/Laru_larichufli.jpeg";
 
 const teamNetworks = [
   {
@@ -17,17 +16,25 @@ const teamNetworks = [
     networkUrl: "https://twitter.com/nanovazquez__",
   },
   {
+    name: "Lara Mateo",
+    username: "larichufli",
+    image: laraProfileImg,
+    networkUrl: "https://twitter.com/larichufli",
+  },
+  /* {
     name: "Nicole Tevez",
     username: "naniitevez",
     image: nickyProfileImg,
     networkUrl: "https://twitter.com/naniitevez",
   },
+
   {
     name: "Mateo Silguero",
     username: "mateosilguero1",
     image: mateProfileImg,
     networkUrl: "https://twitter.com/mateosilguero1",
   },
+    */
 ];
 
 export default teamNetworks;
